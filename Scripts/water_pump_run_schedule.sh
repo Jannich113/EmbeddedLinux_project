4f400d0527d3aade2a417a5e728e002aa087609c
jannich113 <sejejannich@gmail.com>
2023-05-30 13:09:43 +0200

    added mqtt support to time scheduling script

--- Scripts/water_pump_run_schedule.sh
+++ Scripts/water_pump_run_schedule.sh
@@ -5,9 +5,7 @@ current_hour=$(date +%H)
 
 if [ "$current_hour" == "12:00" ] || [ "$current_hour" == "24:00" ]; then
     echo "scheduled watering at: $current_hour"
-    exit true
-else
-    exit false
+    mosquitto_pub -h localhost -t remote/button/pushed -m "p" -u pi -P burgerking
 fi
 
    
